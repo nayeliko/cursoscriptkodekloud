228b6a07bdfffce7e15c36e2f3e232447cc79f0f
Nayel Ebri <nayelebri397@gmail.com>
2025-09-23 18:04:01 +0000

    prueba

---
+++ unset.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+name="JuanCarlos"
+#last_name="Royo"
+
+echo "My full name is ${name} ${last_name}. Nice to meet you."
+
+exit 0
---
+++ unset2.sh
@@ -0,0 +1,8 @@
+#1/usr/bin/env bash
+set -u
+
+name="JuanCarlos"
+
+echo "My full name is ${name} ${last_name}. Nice to meet you."
+
+exit 0
